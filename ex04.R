8416b93527bf4ba424b50410514f1b99b2970300
sjna0730 <57140291+sjna0730@users.noreply.github.com>
2024-05-24 19:18:34 +0900

    2024-05-24 R수업 초급 1-2 7차시 실습
    파일 데이터 불러오는 방법과 R에서의 데이터를 외부로 저장하는 방법

--- ex04.R
+++ ex04.R
@@ -1,36 +1,44 @@
-#리스트(벡터와 같은 1차원 데이터를 나타내는 것은 맞으나
-#리스트는 다른 데이터형을 가질 수 있는것이 리스트임)
+# 외부데이터를 불러오는 방법을 습득
 
-ds <- c(10,15,80,69)
-my.info <- list(name = "홍길동", age = 12, status = TRUE, score = ds)
-my.info
-# 리스트 요소를 접근이나 출력하는 방법
-my.info[1]
-my.info[3]
-my.info$age # $를 사용하는 방법 권장
-my.info$score
-# unlist() = 리스트를 해제하고 벡터식으로 변경함
-vmyinfo <- unlist(my.info)
-vmyinfo
-# as.integer() = 다른 데이터형을 숫자형으로 바꿔주는 함수
-v1 = as.integer(vmyinfo[2])
-v1
+# 1. csv 라는 확장자를 지닌 파일을 읽어와서 데이터프레임에 저장
+# csv(comma seperated values) 확장자는 범용적으로 많이 사용
+setwd("D:|source") # 작업폴더 지정하는 방법
 
-# 팩터(factor)
-bt <- c("A", "B", "B", "O", "AB", "A")
-bt
-fbt <- factor(bt)
-fbt
-bt[5]
-fbt[5]
-# levels() = 팩터에 저장된 데이터의 종류를 알려준다.
-levels(fbt)
+df_exam <- read.csv("csv_exam.csv", header = T) # 작업폴더 안에 해당 파일을 읽어옴
+df_exam
 
-as.integer(fbt) #숫자형으로 변형
-length(fbt)
-fbt[7] <- "B"
-fbt
-# Z라는 값은 fbt 라는 팩터의 level에 없는 값이다.
-# 그래서 NA를 생성한다.
-fbt[8] <- "Z"
-fbt
+# View() 데이터를 좀더 가독성을 좋게 만들어주는 함수 V 대문자
+View(df_exam)
+
+# rm() 은 remove 의 약자로 변수값을 삭제해줌
+rm(df_exam)
+
+# 내장데이터셋인 iris 의 일부데이터만 추출하여 외부데이터로
+# 내보내는 과정
+setwd("C:/R작업/Practice_Chap03(매트릭스와 데이터프레임)")
+setoas <- subset(x=iris, Species=="setosa")
+setosa
+class(setosa)
+write.csv(x=setosa, "setosa.csv", row.names = F)
+
+# 아래와 같이 excel 파일을 R로 가져오기 위해서는
+# readxl 패키지를 인스톨하고, library 함수를 통해서
+# readxl을 쓰겠다고 로딩하는 방법이다.
+install.packages("readxl")
+library(readxl)
+df_excel <- read_excel("excel_exam.xlsx")
+View(df_exam)
+head(df_exam)
+class(df_exam)
+
+mean(df_exam$math) # 평균 구하기
+mean(df_exam$science)
+
+# R에서 조작한 데이터를 외부 엑셀파일로 내보내기를 위해선
+# xlsx 패키지를 설치하고 로딩해야한다.
+install.packages("xlsx")
+library(xlsx)
+
+setosa
+# row.names = F 를 하면 엑셀에서 첫번째 열이 없어짐짐
+write.xlsx(x=setosa, "setosa.xlsx", row.names = F)
